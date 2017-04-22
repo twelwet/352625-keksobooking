7b8c3e3357f4f5c2264b2f0debf188556968f417
Maxim Akimov <akimovmax@mail.ru>
2017-04-22 11:23:15 +0300

    Есть вопрос по координатам '.pin__main'

--- js/map.js
+++ js/map.js
@@ -48,4 +48,48 @@
     }
   });
 
+  // Найдем перетаскиваемый элемент '.pin__main'
+  // Делаю как в демке, только у меня 'за что тащим' и 'что перетакскиваем' совпадает
+  var pinHandle = document.querySelector('.pin__main');
+
+  pinHandle.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      pinHandle.style.top = (pinHandle.offsetTop - shift.y) + 'px';
+      pinHandle.style.left = (pinHandle.offsetLeft - shift.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+      // [ВОПРОС] Даже просто при клике на разные места '.pin__main'
+      // в консоль выводятся чуть разные координаты, как сделать так,
+      // чтобы туда выводились координаты острого конца пина?
+      console.log (startCoords);
+
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+
+  });
 })();
